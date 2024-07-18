addbe675e319563187dd981fa1436a2c2ff774ba
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-18 21:20:57 +0200

    link and router navigation examples

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+"use client";
 import Image from "next/image";
-
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+// redirect for server component
 export default function Home() {
+  const router = useRouter();
+  const [first, setFirst] = useState("");
+  console.log("client component");
+
+  const handleNavigation = () => {
+    router.push("/posts");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Homepage
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <h1>Homepage</h1>
+      <Link href={"/profile"}>Link to Profile</Link>
+      <br />
+      <button onClick={handleNavigation}>
+        Link to Post using useRouter hooks
+      </button>
     </main>
   );
 }
