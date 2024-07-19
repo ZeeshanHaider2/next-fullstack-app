774be8d3ab21c1ad02d1fd99f9d8d2e473021e42
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-20 00:25:07 +0200

    fetching API in server component

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,8 +1,28 @@
 import React from "react";
 
-const PostsPage = ({ searchParams }) => {
-  console.log("searchParams from server component", searchParams.search);
-  return <div>PostsPage</div>;
+async function getData() {
+  const res = await fetch("https://dummyjson.com/posts", {
+    cache: "no-cache",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const PostsPage = async ({ searchParams }) => {
+  //console.log("searchParams from server component", searchParams.search);
+  //return <div>PostsPage</div>;
+  const data = await getData();
+  console.log("data", data);
+  return (
+    <div>
+      {data.posts.map((post, id) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default PostsPage;
